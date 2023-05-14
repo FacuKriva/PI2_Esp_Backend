5d9fbae4a11017ba0c936a1c0b3a710b7c923bf1
royalejandro09 <roymartinezv@gmail.com>
2023-05-14 00:19:14 -0500

    agrego Api-gateway msvc

---
+++ msvc-gateway-api-sprgbt/src/main/java/com/msvc/gateway/api/sprgbt/security/SpringSecurityConfig.java
@@ -0,0 +1,28 @@
+package com.msvc.gateway.api.sprgbt.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+public class SpringSecurityConfig {
+
+    @Autowired
+    private JwtAuthenticationFilter authenticationFilter;
+
+    @Bean
+    public SecurityWebFilterChain configure(ServerHttpSecurity http) {
+        return http.authorizeExchange()
+                .pathMatchers("/security/oauth/**").permitAll()
+                .pathMatchers("/users/**").hasRole("ADMIN")
+                .pathMatchers(HttpMethod.POST,"/api/v1/upload").hasRole("ADMIN")
+                .anyExchange().authenticated()
+                .and().addFilterAt(authenticationFilter, SecurityWebFiltersOrder.AUTHENTICATION)
+                .csrf().disable()
+                .build();
+    }
+}
