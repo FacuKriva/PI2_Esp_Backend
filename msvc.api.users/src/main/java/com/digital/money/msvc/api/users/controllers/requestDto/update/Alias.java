6303a7cb4386ece19435dbd37b320c5839fce1cf
royalejandro09 <roymartinezv@gmail.com>
2023-05-24 23:59:46 -0500

    creacion de usuario con cuenta asociada

--- msvc.api.users/src/main/java/com/digital/money/msvc/api/users/controllers/requestDto/update/Alias.java
+++ msvc.api.users/src/main/java/com/digital/money/msvc/api/users/controllers/requestDto/update/Alias.java
@@ -8,15 +8,15 @@ import lombok.Getter;
 public class Alias {
 
     @NotNull(message = "The first word cannot be null or empty")
-    @Size(min = 6, max = 10, message = "The word must contain a minimum of 6 and a maximum of 1 characters")
+    @Size(min = 6, max = 12, message = "The word must contain a minimum of 6 and a maximum of 12 characters")
     private String wordIndexOne;
 
     @NotNull(message = "The second word cannot be null or empty")
-    @Size(min = 6, max = 10, message = "The word must contain a minimum of 6 and a maximum of 1 characters")
+    @Size(min = 6, max = 12, message = "The word must contain a minimum of 6 and a maximum of 12 characters")
     private String wordIndexZero;
 
     @NotNull(message = "The third word cannot be null or empty")
-    @Size(min = 6, max = 10, message = "The word must contain a minimum of 6 and a maximum of 1 characters")
+    @Size(min = 6, max = 12, message = "The word must contain a minimum of 6 and a maximum of 12 characters")
     private String wordIndexTwo;
 
     public String buildAlias() {
