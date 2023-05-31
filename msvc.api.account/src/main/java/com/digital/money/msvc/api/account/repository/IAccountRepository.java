93339eb852c4a7769ede38b5eb09bcddc7c4336c
FacuKriva <facukriva2001@gmail.com>
2023-05-31 01:09:26 -0300

    Agregamos la función de buscar cuenta por id

--- msvc.api.account/src/main/java/com/digital/money/msvc/api/account/repository/IAccountRepository.java
+++ msvc.api.account/src/main/java/com/digital/money/msvc/api/account/repository/IAccountRepository.java
@@ -10,6 +10,7 @@ import java.util.Optional;
 @Repository
 public interface IAccountRepository extends JpaRepository<Account, Long> {
     Optional<Account> findByCvu(String cvu);
+    Optional<Account> findById(Long id);
     Optional<Account> findByAlias(String alias);
     @Query("SELECT a FROM Account a WHERE a.alias = ?1 AND a.accountId != ?2")
     Optional<Account> aliasUnique(String alias, long id);
