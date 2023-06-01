3bbcc7c28fcf648785f497f6d54caa8fc6cad0f7
afidal <analaurafidalgo@gmail.com>
2023-06-01 17:19:36 -0300

    Agrego un nuevo usuario sin transacciones

--- msvc.api.account/src/main/resources/import.sql
+++ msvc.api.account/src/main/resources/import.sql
@@ -2,6 +2,7 @@
 INSERT INTO accounts (account_id, cvu, alias, available_balance, user_id) VALUES (null, '1828142364587587491111', 'accion.adeudar.afianzamiento',400000.0, 1);
 INSERT INTO accounts (account_id, cvu, alias, available_balance, user_id) VALUES (null, '1828142364587587493333', 'afectacion.divisa.cambios',220000.0, 2);
 INSERT INTO accounts (account_id, cvu, alias, available_balance, user_id) VALUES (null, '1828142364587587495555', 'riqueza.dineros.devaluacion',120000.0, 3);
+INSERT INTO accounts (account_id, cvu, alias, available_balance, user_id) VALUES (null, '2818991716171719171717', 'lampazo.gorila.bufanda',233450.0, 4);
 
 -- Insert Transactions
 INSERT INTO transactions (transaction_id, amount, realization_date, description, from_cvu, to_cvu, type, account_id) VALUES (null, 100000.0, '2023-05-10 08:30:20', 'initial transaction', '1828142364587587491111', 1828142364587587493333, 'OUTGOING', 1);
